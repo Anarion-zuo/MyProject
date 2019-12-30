d285961f0469bd8dd2b29104debd858c82f2895e
Anarion <untrac10100@gmail.com>
2019-12-30 22:37:32 +0800

    sync

---
+++ referencelib/cmake-build-release/CMakeFiles/referencelib.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/anarion/Documents/MyProjects/referencelib/allocator/Allocator.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/allocator/Allocator.cpp.o"
+  "/Users/anarion/Documents/MyProjects/referencelib/base/HashFunctions.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/base/HashFunctions.cpp.o"
+  "/Users/anarion/Documents/MyProjects/referencelib/base/Object.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/base/Object.cpp.o"
+  "/Users/anarion/Documents/MyProjects/referencelib/concurrent/base/CondVar.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/concurrent/base/CondVar.cpp.o"
+  "/Users/anarion/Documents/MyProjects/referencelib/concurrent/base/Mutex.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/concurrent/base/Mutex.cpp.o"
+  "/Users/anarion/Documents/MyProjects/referencelib/concurrent/base/Thread.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/concurrent/base/Thread.cpp.o"
+  "/Users/anarion/Documents/MyProjects/referencelib/container/SString.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/container/SString.cpp.o"
+  "/Users/anarion/Documents/MyProjects/referencelib/gc/GarbageCollector.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/gc/GarbageCollector.cpp.o"
+  "/Users/anarion/Documents/MyProjects/referencelib/io/Buffer/Buffer.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/io/Buffer/Buffer.cpp.o"
+  "/Users/anarion/Documents/MyProjects/referencelib/main.cpp" "/Users/anarion/Documents/MyProjects/referencelib/cmake-build-release/CMakeFiles/referencelib.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ referencelib/cmake-build-release/CMakeFiles/referencelib.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/referencelib.dir/allocator/Allocator.cpp.o"
+  "CMakeFiles/referencelib.dir/base/HashFunctions.cpp.o"
+  "CMakeFiles/referencelib.dir/base/Object.cpp.o"
+  "CMakeFiles/referencelib.dir/concurrent/base/CondVar.cpp.o"
+  "CMakeFiles/referencelib.dir/concurrent/base/Mutex.cpp.o"
+  "CMakeFiles/referencelib.dir/concurrent/base/Thread.cpp.o"
+  "CMakeFiles/referencelib.dir/container/SString.cpp.o"
+  "CMakeFiles/referencelib.dir/gc/GarbageCollector.cpp.o"
+  "CMakeFiles/referencelib.dir/io/Buffer/Buffer.cpp.o"
+  "CMakeFiles/referencelib.dir/main.cpp.o"
+  "referencelib"
+  "referencelib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/referencelib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
